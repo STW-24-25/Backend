fd705af71facb1946964893d1107a8f83752cc76
Str1ien <villagrasaalain@gmail.com>
2025-03-25 23:40:22 +0100

    added swagger to /products

--- src/middleware/validator/product.schemas.ts
+++ src/middleware/validator/product.schemas.ts
@@ -1,1 +1,44 @@
 import { z } from 'zod';
+
+
+/**
+ * @swagger
+ * components:
+ *  parameters:
+ *    getAllProductsNameParameterSchema:
+ *      in: query
+ *      name: name
+ *      schema:
+ *        type: string
+ *      required: false
+ *      description: Name of the product to retrieve, empty for all products
+ *    getAllProductsPageParameterSchema:
+ *      in: query
+ *      name: page
+ *      schema:
+ *        type: integer
+ *        minimum: 1
+ *        default: 1
+ *      required: true
+ *      description: Page to be retrieved
+ *    getAllProductsSizeParameterSchema:
+ *      in: query
+ *      name: size
+ *      schema:
+ *        type: integer
+ *        minimum: 1
+ *        default: 16
+ *      required: true
+ *      description: Number of products per page
+ */
+export const getAllProductsSchema = z.object({
+  query: z.object({
+    name: z.string().optional(),
+    page: z.string().optional().refine((val) => !isNaN(Number(val)) && Number(val) > 0, {
+      message: "Page must be a positive number",
+    }),
+    size: z.string().optional().refine((val) => !isNaN(Number(val)) && Number(val) > 0, {
+      message: "Size must be a positive number",
+    }),
+  }),
+});
--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -6,7 +6,51 @@ const router = Router();
 
 // #### PUBLIC ####
 
-// todo swagger
+/**
+ * @swagger
+ * /api/products:
+ *  get:
+ *    summary: Return products basic information (paginated). Get a list of products with optional filtering and pagination, including the last known price
+ *    tags: [Product]
+ *    parameters:
+ *      - $ref: '#/components/parameters/getAllProductsNameParameterSchema'
+ *      - $ref: '#/components/parameters/getAllProductsPageParameterSchema'
+ *      - $ref: '#/components/parameters/getAllProductsSizeParameterSchema'
+ *    responses:
+ *      200:
+ *        description: A paginated list of products with the last known price
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                products:
+ *                  type: array
+ *                  items:
+ *                    type: object
+ *                    properties:
+ *                      _id:
+ *                        type: string
+ *                      name:
+ *                        type: string
+ *                      sector:
+ *                        $ref: '#/components/schemas/ProductSector'
+ *                      lastPrice:
+ *                        type: number
+ *                        format: float
+ *                      image:
+ *                        type: string
+ *                page:
+ *                  type: integer
+ *                pageSize:
+ *                  type: integer
+ *                totalProducts:
+ *                  type: integer
+ *                totalPages:
+ *                  type: integer
+ *      500:
+ *        description: Error processing request
+ */
 router.get('/', productCont.getAllProducts);
 
 // todo swagger
